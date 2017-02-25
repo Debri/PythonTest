599eac3bd1c6411e7d60527255607d033b574de5
Debri <18908356464@163.com>
2017-02-25 16:48:47 +0800

    first

--- com/geek/__init__.py
+++ com/geek/__init__.py
@@ -8,3 +8,14 @@ print(1)
 tinydict = {'name': 'john','code':6734, 'dept': 'sales'}
 print(tinydict['code'])
 
+
+i = 2
+while(i < 100):
+    j = 2
+    while(j <= (i/j)):
+        if not(i%j): break
+        j = j + 1
+    if (j > i/j) : print (i), " 是素数"
+    i = i + 1
+
+print ("good bye")
