c1661b8af21e481e48073e76cc2c90b922ed0094
Debri <18908356464@163.com>
2017-02-25 16:13:43 +0800

    first

---
+++ com/geek/__init__.py
@@ -0,0 +1,10 @@
+print("hello world")
+if True:
+    print("true")
+else:
+    print("false")
+print("123")
+print(1)
+tinydict = {'name': 'john','code':6734, 'dept': 'sales'}
+print(tinydict['code'])
+
